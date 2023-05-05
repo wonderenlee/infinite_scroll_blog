05bc279f1049405ea989f0fa713d85cc2a57ce6a
wonderenlee <wonderenlee@gmail.com>
2023-05-05 15:05:19 -0400

    troubleshoot for filter

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ async function getPosts() {
 async function showPosts() {
     const posts = await getPosts();
 
-    posts.forEach(posts => {
+    posts.forEach(post => {
         const postEl = document.createElement('div');
         postEl.classList.add('post');
         postEl.innerHTML = `
-            <div class="number">${posts.id}</div>
+            <div class="number">${post.id}</div>
             <div class="post-info">
-                <h2 class="post-title">${posts.title}</h2> 
-                <p class="post-body">${posts.body}</p>
+                <h2 class="post-title">${post.title}</h2> 
+                <p class="post-body">${post.body}</p>
             </div>
         `;
 
@@ -49,12 +49,12 @@ function showLoading() {
 
 // Filter posts by input
 function filterPosts(e) {
-    const term = e.target.value.toUppercase();
+    const term = e.target.value.toUpperCase();
     const posts = document.querySelectorAll('.post');
 
     posts.forEach(post => {
-        const title = post.querySelector('.post-title').innerText.toUppercase();
-        const body = post.querySelector('.post-body').innerText.toUppercase();
+        const title = post.querySelector('.post-title').innerText.toUpperCase();
+        const body = post.querySelector('.post-body').innerText.toUpperCase();
 
         if(title.indexOf(term) > -1 || body.indexOf(term) > -1) {
             post.style.display = 'flex';
@@ -71,7 +71,7 @@ showPosts();
 window.addEventListener('scroll', () => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-    if (scrollTop + clientHeight >= scrollHeight - 5) {
+    if (scrollTop + clientHeight === scrollHeight) {
         showLoading();
     }
 });
